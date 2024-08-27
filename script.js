0199c8d275551ce4946851864499f9cf60875285
mandarmgd <165933246+mandarmgd@users.noreply.github.com>
2024-08-28 01:21:34 +0530

    Add squares to the grid

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+
+const grid = document.querySelector('.grid');
+let gridHeight = parseInt(getComputedStyle(grid).height);
+let gridWidth = parseInt(getComputedStyle(grid).width);
+const gridArea = gridHeight * gridWidth;
+let sqSide = 16;
+let sqCount = sqSide * sqSide;
+let sqHeight = gridHeight / sqSide;
+let sqWidth = gridWidth / sqSide;
+
+for (let i = 1; i <= sqCount; i++) {
+  const square = document.createElement('div');
+  square.style.height = `${sqHeight}px`;
+  square.style.width = `${sqWidth}px`;
+  square.classList.add('square');
+  grid.insertAdjacentElement('beforeend', square);
+}
