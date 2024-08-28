95e20ebd5024523761e85b472b73baec2a8b1337
mandarmgd <165933246+mandarmgd@users.noreply.github.com>
2024-08-28 22:19:12 +0530

    Made the squares darken upon hovering multiple times

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ let gridHeight = parseInt(getComputedStyle(grid).height);
 let gridWidth = parseInt(getComputedStyle(grid).width);
 const gridArea = gridHeight * gridWidth;
 
+const addHover = e => {
+  if (e.target.classList.contains('square')) {
+    e.target.style.opacity = String(+e.target.style.opacity + 0.1);
+    const c1 = Math.trunc(Math.random() * 256);
+    const c2 = Math.trunc(Math.random() * 256);
+    const c3 = Math.trunc(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+    console.log(e.target);
+  }
+};
+const bindHover = addHover.bind(this);
+
 const setSquares = numSq => {
   grid.replaceChildren();
-
   const sqSide = numSq;
   const sqCount = sqSide * sqSide;
   const sqHeight = gridHeight / sqSide;
@@ -17,6 +28,7 @@ const setSquares = numSq => {
     const square = document.createElement('div');
     square.style.height = `${sqHeight}px`;
     square.style.width = `${sqWidth}px`;
+    square.style.opacity = '0';
     square.classList.add('square');
     grid.insertAdjacentElement('beforeend', square);
     /*
@@ -34,15 +46,7 @@ const setSquares = numSq => {
     }
     */
   }
-
-  grid.addEventListener('mouseover', function (e) {
-    if (e.target.classList.contains('square')) {
-      const c1 = Math.trunc(Math.random() * 256);
-      const c2 = Math.trunc(Math.random() * 256);
-      const c3 = Math.trunc(Math.random() * 256);
-      e.target.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
-    }
-  });
+  grid.addEventListener('mouseover', bindHover);
 };
 setSquares(16);
 
@@ -50,5 +54,6 @@ const reset = document.querySelector('button');
 reset.addEventListener('click', () => {
   const num = +prompt('Enter the number of squares per side');
   if (num <= 0 || num > 100) return;
+  grid.removeEventListener('mouseover', bindHover);
   setSquares(num);
 });
